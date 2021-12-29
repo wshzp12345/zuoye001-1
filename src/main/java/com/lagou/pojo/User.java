63796e5ac691bb8f50506fd89f81b7fd77a1a357
albert.hou <hzphlm@163.com>
2021-12-29 22:52:29 +0800

    Initial commit

---
+++ src/main/java/com/lagou/pojo/User.java
@@ -0,0 +1,36 @@
+package com.lagou.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Integer id;
+    private String username;
+//    private String password;
+//    private String birthday;
+
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "id=" + id +
+//                ", username='" + username + '\'' +
+//                '}';
+//    }
+}
